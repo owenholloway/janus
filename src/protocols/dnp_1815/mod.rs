ba784ef90df09cb1509b097990ee7cfa7ae078ec
Owen Holloway <owen.holloway@lanceolata.com.au>
2023-07-05 21:34:39 +1000

    object generic

--- src/protocols/dnp_1815/mod.rs
+++ src/protocols/dnp_1815/mod.rs
@@ -1,4 +1,5 @@
 pub mod function_code;
 pub mod point;
+pub mod object;
 pub mod request;
 pub mod response_objects;
---
+++ src/protocols/dnp_1815/object.rs
@@ -0,0 +1,6 @@
+pub struct ObjectHeader {
+    pub group: u8,
+    pub variation: u8,
+    pub qualifier: u8,
+    pub range: Vec<u8>,
+}
