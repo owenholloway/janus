307d1951acda5ae96757101e4494a9eff5551f2e
Owen Holloway <owen.holloway@lanceolata.com.au>
2023-07-18 22:21:44 +1000

    add bench

---
+++ benches/modbus/mod.rs
@@ -0,0 +1,56 @@
+use janus::protocols::modbus::{
+    data::{coil::{Coil, CoilValue}, discrete_input::{DiscreteInput, DiscreteInputValue}},
+    device::{create_device, Device},
+    program_data_unit::{ProtocolDataUnitRequest, ReadCoilsRequest, ReadDiscreteInputsRequest},
+    read_data::ReadData,
+};
+
+pub fn bench_coil_processing(coil_count: u16) {
+    let mut device: Device = create_device();
+
+    for index in 0..coil_count {
+        device.coils[index as usize] = Coil::EnabledReadOnly {
+            coil_value: CoilValue(true),
+        };
+    }
+
+    let pdu: ProtocolDataUnitRequest =
+        ProtocolDataUnitRequest::ReadCoilsRequest(ReadCoilsRequest {
+            starting_address: 0,
+            quantity_of_coils: coil_count,
+        });
+
+    let result = device.process_request(pdu);
+
+    match result {
+        Ok(_) => {
+            assert!(true)
+        }
+        Err(_) => assert!(false),
+    }
+}
+
+pub fn bench_discrete_input_processing(discrete_input_count: u16) {
+    let mut device: Device = create_device();
+
+    for index in 0..discrete_input_count {
+        device.discrete_inputs[index as usize] = DiscreteInput::EnabledReadOnly {
+            discrete_value: DiscreteInputValue(true),
+        };
+    }
+
+    let pdu: ProtocolDataUnitRequest =
+        ProtocolDataUnitRequest::ReadDiscreteInputsRequest(ReadDiscreteInputsRequest {
+            starting_address: 0,
+            quantity_of_inputs: discrete_input_count,
+        });
+
+    let result = device.process_request(pdu);
+
+    match result {
+        Ok(_) => {
+            assert!(true)
+        }
+        Err(_) => assert!(false),
+    }
+}
